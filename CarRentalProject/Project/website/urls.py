39306edd917bec63b14b87a10a62e9cba05bbd66
Adib Akkari <adibakkari@gmail.com>
2024-03-06 22:00:55 -0500

    Adding,editing,deleting records and register user

--- CarRentalProject/Project/website/urls.py
+++ CarRentalProject/Project/website/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.urls import path
 from . import views  # Import your views module
 
+
 urlpatterns = [
     path('', views.home, name='home'),  # Define URL pattern for the home view
-   # path('login/', views.login_user, name='login'),
+    path('login/', views.home, name='login'),  # Use the home view for login
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('record/<int:pk>', views.customer_record, name='record'),
+    path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
+    path('add_record/', views.add_record, name='add_record'),
+    path('update_record/<int:pk>', views.update_record, name='update_record'),
 ]
-
