a945a803b8e652d5595169d3e9ba6fb7abe77b94
Adib Akkari <adib.akkari@ntint.com>
2024-03-31 18:27:37 -0400

    fixing a bug while merging

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -3,16 +3,13 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import LoginScreen from './screens/LoginScreen';
-<<<<<<< HEAD
-import ReservationListScreen from './screens/ReservationListScreen';
-=======
+
 import ReservationListScreen from './screens/ReservationListScreen'; 
 
 
 
 import BranchProducts from './components/BranchProducts';
 
->>>>>>> 8e1cb89d6ad7eaea88fc5570568cd2de2520bbca
 import ReservationForm from './components/ReservationForm';
 import CartScreen from './screens/CartScreen';
 import ProfileScreen from './screens/ProfileScreen';
@@ -57,17 +54,12 @@ function App() {
               
               <Route path='/admin/productlist' element={<ProductListScreen/>} />
               
-<<<<<<< HEAD
-              <Route path='/admin/product/:id/edit' element={<ProductEditScreen/>} />
-          </Routes>
-=======
               <Route path='/admin/product/:id/edit' element={<ProductEditScreen/>} />   
 
               <Route path="/branch/:branchId" element={<BranchProducts />} />
 
 
             </Routes>
->>>>>>> 8e1cb89d6ad7eaea88fc5570568cd2de2520bbca
         </Container>
       </main>
       <Footer/>
